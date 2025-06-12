2a811842cf88113264398246b2d7006b961693f2
laboratorio <samir.cuenca@udla.edu.ec>
2025-06-12 14:46:24 -0500

    implementacion de la clase arista

---
+++ src/Vertice.java
@@ -0,0 +1,16 @@
+public class Vertice {
+    private String id;
+    private int x, y;
+    public Vertice(String id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+    public String getId() { return id; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    @Override
+    public String toString() {
+        return id + " (" + x + "," + y + ")";
+    }
+}
